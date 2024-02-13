c6d34827336b93971d1993f3fb0a2dffe46de73c
William Lin <linwilliam14@gmail.com>
2024-02-13 01:03:37 +0100

    Version 12

--- site/sum.test.js
+++ site/sum.test.js
@@ -1,9 +1,16 @@
 const { JSDOM } = require('jsdom');
-const { renderCartItems } = require('./sum'); // Assurez-vous d'ajuster le chemin d'importation en fonction de votre structure de fichiers
+const { renderCartItems } = require('./sum');
 
 test('renderCartItems affiche correctement les articles dans le panier', () => {
   // Créez un élément de conteneur factice pour le test
   const dom = new JSDOM('<div id="cart-container"></div>');
+
+  // Mock des données du panier
+  const mockCart = [
+    { name: 'Article 1', price: 10, quantity: 2 },
+    { name: 'Article 2', price: 20, quantity: 1 },
+    // ... ajoutez d'autres articles au besoin
+  ];
 
   // Exécutez la fonction à tester
   renderCartItems(mockCart);
